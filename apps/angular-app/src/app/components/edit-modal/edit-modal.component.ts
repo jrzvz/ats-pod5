2aae41ffd2b1e8951c3db6c5a8ac85eb9494ab0e
Rodriguez Valdez <j.rodriguez.valdez@accenture.com>
2024-10-23 11:48:13 -0600

    add edit-modal component

---
+++ apps/angular-app/src/app/components/edit-modal/edit-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mfee-project-edit-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './edit-modal.component.html',
+  styleUrl: './edit-modal.component.scss'
+})
+export class EditModalComponent {}
