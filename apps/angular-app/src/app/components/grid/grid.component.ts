d476c2ea5f6daa6aa4d6d66cb7b56f17eb92492c
Rodriguez Valdez <j.rodriguez.valdez@accenture.com>
2024-10-24 13:41:41 -0600

    use ngFor for adding cards on grid.component

--- apps/angular-app/src/app/components/grid/grid.component.ts
+++ apps/angular-app/src/app/components/grid/grid.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'mfee-project-grid',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.scss'
 })
