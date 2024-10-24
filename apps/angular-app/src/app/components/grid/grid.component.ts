27f7baaefbf4f2ce7a74582773a19b906292bedc
Rodriguez Valdez <j.rodriguez.valdez@accenture.com>
2024-10-24 12:51:25 -0600

    add grid component

---
+++ apps/angular-app/src/app/components/grid/grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mfee-project-grid',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './grid.component.html',
+  styleUrl: './grid.component.scss'
+})
+export class GridComponent {}
