86219c4b3a8927c770bf40a3678b92c8c0c207e3
Rodriguez Valdez <j.rodriguez.valdez@accenture.com>
2024-10-24 11:11:23 -0600

    add input with 2way-binding on edit-component

--- apps/angular-app/src/app/components/edit-modal/edit-modal.component.ts
+++ apps/angular-app/src/app/components/edit-modal/edit-modal.component.ts
@@ -1,13 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'mfee-project-edit-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-modal.component.html',
   styleUrl: './edit-modal.component.scss'
 })
 export class EditModalComponent {
-  @Input() modalText: string = '';
+  @Input() modalText: string;
+  @Output() modalTextChange = new EventEmitter<string>();
+
+  updateModalText(): void {
+    this.modalTextChange.emit(this.modalText);
+  }
 }
