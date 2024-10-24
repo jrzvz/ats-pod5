6b838611d2c3078b6cf10e363110a6ea24a1573f
Rodriguez Valdez <j.rodriguez.valdez@accenture.com>
2024-10-24 11:15:21 -0600

    add header, card and edit-modal components on app.component

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
+import { EditModalComponent } from './components/edit-modal/edit-modal.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent, CardComponent, EditModalComponent],
   selector: 'mfee-project-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
