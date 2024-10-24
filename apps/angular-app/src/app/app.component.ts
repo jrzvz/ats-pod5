7466d645b10727fb21b7b43bd2b4ec4b86a370e5
Rodriguez Valdez <j.rodriguez.valdez@accenture.com>
2024-10-24 13:29:08 -0600

    use grid component on app.component instead of card

--- apps/angular-app/src/app/app.component.ts
+++ apps/angular-app/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CardComponent } from './components/card/card.component';
 import { EditModalComponent } from './components/edit-modal/edit-modal.component';
+import { GridComponent } from './components/grid/grid.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent, CardComponent, EditModalComponent],
+  imports: [RouterModule, HeaderComponent, CardComponent, EditModalComponent, GridComponent],
   selector: 'mfee-project-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
